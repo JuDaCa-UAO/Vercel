f90fc60d166a53be26eb9c9625c0becfb48a7762
JuDaCa-UAO <juan_david.carvajal@uao.edu.co>
2024-09-29 13:47:47 -0500

    implementacion js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello!');
+function compararNumeros(num1, num2) {
+    if (num1 === num2) {
+        console.log("Los números son iguales.");
+    } else if (num1 > num2) {
+        console.log(num1 + " es mayor que " + num2);
+    } else {
+        console.log(num1 + " es menor que " + num2);
+    }
+}
+
+compararNumeros(5, 10);
+compararNumeros(8, 3);
+compararNumeros(7, 7);
